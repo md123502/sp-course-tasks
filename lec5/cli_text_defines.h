d9442a76eb3b7842cd0d296c0a80234ddd6c4e8c
md123502 <dyagmak@gmail.com>
2023-11-18 00:08:48 +0700

    Task 5

---
+++ lec5/cli_text_defines.h
@@ -0,0 +1,12 @@
+#ifndef CLI_TEXT_DEFINES_H
+#define CLI_TEXT_DEFINES_H
+
+#define INPUT_HINT "Need a single argument - user login\n"
+#define CANNOT_OPEN_PASSWD_FILE_MSG "Cannot open passwd file\n"
+#define CANNOT_GET_USERNAME_MSG "Cannot get username:\n"
+#define USER_NOT_FOUND_MSG "\tno such user\n"
+#define MEMORY_ALLOCATION_ERROR_MSG "\tmemory allocation error\n"
+#define READING_ERROR_MSG "\treading error\n"
+#define UNEXPECTED_EOF_MSG "\tfile ends where it shouldn't\n"
+
+#endif
